1b85203fa2f9be0d09edbf9622ad8854706be211
thanhtuan <thanhtuancristiano@gmail.com>
2022-11-15 14:36:31 +0700

    update add list hotmail
    update add list hotmail

--- controller/hotmail.controller.js
+++ controller/hotmail.controller.js
@@ -2,9 +2,10 @@ let Data_checked = require('../model/data_checked')
 let Data_not_checked = require('../model/data_not_checked')
 let Hotmail = require('../model/hotmail')
 let Account = require('../model/account')
+let fs = require('fs')
 module.exports = {
     add_hotmail: async function (req, res) {
-        console.log('---SERVER HOTMAIL -- API ADD hotmail------')
+        console.log('---SERVER HOTMAIL -- API Add hotmail------')
         try {
             let mail = req.body.mail
             let password = req.body.password
@@ -95,16 +96,31 @@ module.exports = {
         console.log('---SERVER HOTMAIL -- API update hotmail------')
         try {
             let mail = req.body.mail
+            let code2fa = req.body.code2fa
+            let address = req.body.address
+            let name = req.body.name
+            let dob = req.body.dob
             let tags = req.body.tags
             let filter = {
                 mail: mail
             }
+            let findMail = await Hotmail.findOne(filter)
             let update = {
-                $push: {
-                    tags: tags
-                }
+                timeUpdate: new Date(),
+                $set: {
+                    code2fa: code2fa ? code2fa : findMail.code2fa,
+                    name: name ? name : findMail.name,
+                    address: address ? address: findMail.address,
+                    dob: dob ? dob : findMail.dob,
+                    tags: tags ? tags : findMail.tags,
+                },
+                // $push: {
+                //     tags: tags 
+                // }
             }
             let rs_update = await Hotmail.findOneAndUpdate(filter, update, {new: true})
+            console.log(rs_update)
+            console.log(update)
             if(rs_update !== null){
                 res.status(200).json({
                     message: 'Update thanh cong',
@@ -169,6 +185,96 @@ module.exports = {
                 res.status(400).json({
                     message: 'Het phien dang nhap'
                 })
+            }
+        } catch (ex) {
+            res.status(400).json({
+                message: ex.message
+            })
+        }
+    },
+    add_hotmail_white: async function (req, res){
+        console.log('---SERVER HOTMAIL -- API add hotmail white------')
+        try {
+            let mail = req.body.mail
+            let password = req.body.password
+            let mailReco = req.body.mailReco
+            let newMail = new Hotmail({
+                mail: mail,
+                password: password,
+                mailReco: mailReco,
+                status: 1,
+                isdelete: 2,
+                timeAdd: new Date(),
+            })
+            let checkExists = await Hotmail.findOne({mail: mail})
+            if(checkExists ==  null){
+                let save = await newMail.save()
+                if(save !== null){
+                    res.status(200).json({
+                        message: "Them du lieu thanh cong"
+                    })
+                }else{
+                    res.status(400).json({
+                        message: "Them du lieu that bai"
+                    })
+                }
+            }else{
+                res.status(400).json({
+                    message: "Du lieu da ton tai"
+                })
+            }
+        } catch (ex) {
+            res.status(400).json({
+                message: ex.message
+            })
+        }
+    },
+    add_list_hotmail: async function (req, res) {
+        console.log('---SERVER HOTMAIL--- API add list hotmail------')
+        try {
+            let countSucess = 0, countFailed = 0
+            var readData = fs.readFileSync('data.txt', 'utf8')
+            var array = readData.split("\r\n")
+            let rs_data = await Hotmail.find()
+            let map = new Map()
+            for(let i = 0; i < rs_data.length; i++){
+                map.set(rs_data[i].mail, "key")
+            }
+            for(let i = 0; i < array.length; i++){
+                let rss_data = array[i].split("|")
+                let mail = rss_data[0]
+                let password = rss_data[1]
+                let mailReco = rss_data[2]
+                let code2fa = rss_data[3]
+                let name = rss_data[4]
+                let dob = rss_data[5]
+
+                let newData = new Hotmail({
+                    mail: mail,
+                    password: password,
+                    mailReco: mailReco,
+                    code2fa: code2fa,
+                    name: name,
+                    dob: dob,
+                    timeAdd: new Date()
+                })
+                if(map.has(mail) == false){
+                    map.set(mail, "key")
+                    let save = await newData.save()
+                    console.log('Saved  '+  array[i]);
+                    countSucess++
+                }else{
+                    console.log('Failed  '+  array[i]);
+                    countFailed++
+                }
+                if(i+1 == array.length){
+                    res.status(200).json({
+                        message: 'Them du lieu thanh cong.',
+                        total: array.length,
+                        countSucess: countSucess,
+                        countFailed: countFailed,
+                    })
+                }
             }
         } catch (ex) {
             res.status(400).json({
