08b8d0cecdf7b647d22a4da06b213e084609bc22
thanhtuan <thanhtuancristiano@gmail.com>
2022-11-17 11:37:58 +0700

    update
    update

--- model/data_checked.js
+++ model/data_checked.js
@@ -12,6 +12,7 @@ const data_checkedSchema = new mongoose.Schema({
     address: String,
     isdelete: Number,
     status: Number,
+    useStatus: Number, //1 add thanh cong, 2 add that bai, 3 mua thanh cong, 4 mua that bai
     tags: Array,
     timeAdd: Date,
     timeUpdate: Date,
